4f27925f8fab401c8f61aca4173a8db883625ffa
Miftakhul Rizky <mmriz16>
2022-06-15 00:00:31 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum temporibus, aliquid numquam omnis libero magni illo nihil ea nisi impedit, officiis fugit a maxime labore!</p>
 </body>
 </html>
